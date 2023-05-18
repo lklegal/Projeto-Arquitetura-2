3437937e4b6470950dbefa7ce8afa9d5a07eeea7
lklegal <110703055+lklegal@users.noreply.github.com>
2023-05-17 21:20:45 -0300

    Tentando algo novo

---
+++ max.asm
@@ -0,0 +1,47 @@
+.data
+	lista: 	     .word   44, 86, 8, 72, 11, 46, 51, 83, 88, 87
+	limite:      .word   10
+	maior:       .word   0
+
+.text
+	la $t1, lista #endereço de lista[0]
+	lw $t1, 0($t1) #valor de lista[0]
+	la $t2, maior #endereço de maior
+	sw $t1, 0($t2) #maior = lista[0]
+	add $t1, $zero, $zero #i
+for:
+	lw $t2, limite
+	beq $t1, $t2, fim_for #for i in range(limite)
+	la $t2, lista
+	add $t4, $t1, $zero #não vai precisar
+	sll $t1, $t1, 2 #não vai precisar disso, no processador os endereços vão ser de 1 em 1, não de 4 em 4
+	add $t2, $t2, $t1 #endereço de lista[i]
+	add $t1, $t4, $zero #não vai precisar
+	lw $t2, 0($t2) #valor de lista[i]
+	lw $t3, maior
+	sgt $t3, $t2, $t3
+	beq $t3, $zero, fim_if #if lista[i] > maior
+	la $t3, maior
+	sw $t2, 0($t3) #maior = lista[i]
+fim_if:
+	addi $t1, $t1, 1 #i++ #teremos o registrador $one, então a instrução ficaria add $t1, $t1, $one
+	j for
+fim_for:
+	#print(maior)
+	li $v0, 1
+	la $t1, maior
+	lw $a0, 0($t1)
+	syscall
+
+	li $v0, 10
+	syscall
+
+#load: lw(0), la(1)
+#comparação: sgt(2)
+#branch: bne(3), beq(4)
+#store: sw(5)
+#soma: add(6)
+#jump: j(7)
+#5 registradores
+#15 instruções
+#vetor com 10 números
